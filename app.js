06e0a154d5f13fb00c4dc04b98b12d59ee4de7d9
ManuLPGS <manuellaporta@gamestop.com>
2023-08-29 15:09:24 +0200

    set preview

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const dialog = document.getElementById("dialog");
 const preview = document.getElementById("preview");
 const dialogContent = dialog.querySelector(".content");
 const guideInfo = dialogContent.innerHTML;
-const senderSelect = document.getElementById('senderSelect');
-const subjectSelect = document.getElementById('subjectSelect');
-const senderPreview = document.getElementById('senderPreview')
-const subjectPreview = document.getElementById('subjectPreview')
-
+const senderSelect = document.getElementById("senderSelect");
+const subjectSelect = document.getElementById("subjectSelect");
+const senderPreview = document.getElementById("senderPreview");
+const subjectPreview = document.getElementById("subjectPreview");
+const resetFormBtn = document.getElementById("reset");
+const mailerForm = document.getElementById("mailerForm")
 
 const activeDialog = () => {
   closeDialogBtn();
@@ -29,34 +30,39 @@ const closeDialogBtn = () => {
   });
 };
 
-const setSenderPreview = () =>{
-  senderSelect.addEventListener('change', () =>{
-    senderPreview.innerHTML = '<b>From:</b> ' + senderSelect.value + '@gamestop.com'
-  })
-}
-const setSubjectPreview = () =>{
-  subjectSelect.addEventListener('keyup', (e) =>{
-    subjectPreview.innerHTML = '<b>Subject:</b> ' + e.target.value
-  })
-}
+const setSenderPreview = () => {
+  senderSelect.addEventListener("change", () => {
+    senderPreview.innerHTML = ["<b>From:</b> ",senderSelect.value,"@gamestop.com"].join('');
+  });
+};
+const setSubjectPreview = () => {
+  subjectSelect.addEventListener("keyup", (e) => {
+    subjectPreview.innerHTML = ["<b>Subject:</b> ",e.target.value].join('');
+  });
+};
 
-setSubjectPreview()
-
-setSenderPreview()
+const resetForm = () => {
+  mailerForm.addEventListener("change", () => {
+    resetFormBtn.disabled = false;
+    resetFormBtn.addEventListener("click", () => {
+      mailerForm.reset();
+    });
+  });
+};
 
 // playground
+
+setSubjectPreview();
+setSenderPreview();
+resetForm();
 
 info.addEventListener("click", () => {
   dialogContent.innerHTML = guideInfo;
   activeDialog();
 });
 
-preview.addEventListener("click", (e) => {
-  dialogContent.innerHTML = "";
-  dialogContent.innerHTML += '<h2>Mail Content</h2>';
-  dialogContent.innerHTML += preview.innerHTML;
-
+preview.addEventListener("click", () => {
+  dialogContent.innerHTML = ["<h2>Mail Content</h2>",preview.innerHTML].join('\n');
   activeDialog();
-
   closeOnclickingOutsideDialog();
-});
+})
